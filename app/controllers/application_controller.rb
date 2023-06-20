7f5d26162b5bb6146f884480176b434808b83fd2
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-06-20 15:00:58 +0000

    [Add]header,About追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
    #devise利用の機能（ユーザー登録、ログイン認証など）が行われる前に下記のメソッドが実行される
    before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  #初期設定を上書き。　Aboutページへ遷移するよう設定
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  #ログアウト後、Aboutページへ遷移するよう設定
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+
   protected
   #ユーザー登録（sign_up）の際に、ユーザー名（name）のデータ操作を受け取り許可与えるための記述
   def configure_permitted_parameters
