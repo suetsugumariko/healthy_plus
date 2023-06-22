097dcdf4444fcdafccdbd354683c7c2293d6ba1e
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-06-22 18:11:31 +0000

    [Add]customer内容追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,31 @@
 class Public::CustomersController < ApplicationController
 
   def index
+     @customer = Customer.new
+     @customer = Customer.all
+  end
+
+  def create
+    #データを受け取り新規登録するためのインスタンス作成
+    @customer = Customer.new(customer_params)
+    #データをデータベースに保存するためのsaveメソッド実行
+    homes.save
+    #トップ画面へリダイレクト
+    redirect_to '/'
   end
 
   def show
   end
 
   def edit
+     @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    #マイページにリダイレクト
+    redirect_to customers_my_page_path(customer.id)
   end
 
   def unsubscribe
@@ -18,4 +34,15 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+
+
+
+  private
+  #ストロングパラメータ
+  #formから送られてくるデータの中身
+  def list_params
+  #require 送られたデータの中からモデル名を指定しデータを絞り込む
+  #permit requireで絞り込んだデータの中から保存を許可するカラムを指定する
+    params.require(:customer).permit(:guest, :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :start_time, :body_weight, :temperature, :pulse, :max_blood_pressure, :min_blood_pressure, :saturation, :exercise_time, :hours_of_sleep)
+  end
 end
