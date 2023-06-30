14bb38c42e028a4f97cb0c0e9cd2620eb66f622f
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-06-30 15:58:44 +0000

    [Add]admi豆知識新規登録機能追加

--- app/controllers/admin/beauty_nutrition_knowledges_controller.rb
+++ app/controllers/admin/beauty_nutrition_knowledges_controller.rb
@@ -4,9 +4,16 @@ class Admin::BeautyNutritionKnowledgesController < ApplicationController
   end
 
   def new
+    @beauty_nutrition_knowledge = BeautyNutritionKnowledge.new
   end
 
   def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    beauty_nutrition_knowledge = BeautyNutritionKnowledge.new(beauty_nutrition_knowledge_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    beauty_nutrition_knowledge.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def show
@@ -16,5 +23,12 @@ class Admin::BeautyNutritionKnowledgesController < ApplicationController
   end
 
   def update
+  end
+
+
+  private
+  # ストロングパラメータ
+  def beauty_nutrition_knowledge_params
+    params.require(:beauty_nutrition_knowledge).permit(:title, :genre_id, :contrnts)
   end
 end
