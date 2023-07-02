90d427d88a9ac28c37b07af36ce08fe05ee637f1
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-07-02 10:44:50 +0000

    [Add]admi豆知識機能追加

--- app/controllers/admin/beauty_nutrition_knowledges_controller.rb
+++ app/controllers/admin/beauty_nutrition_knowledges_controller.rb
@@ -12,17 +12,28 @@ class Admin::BeautyNutritionKnowledgesController < ApplicationController
     beauty_nutrition_knowledge = BeautyNutritionKnowledge.new(beauty_nutrition_knowledge_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     beauty_nutrition_knowledge.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 4. 詳細画面へリダイレクト
+    redirect_to admin_beauty_nutrition_knowledge_path(beauty_nutrition_knowledge.id)
   end
 
   def show
+     @beauty_nutrition_knowledge = BeautyNutritionKnowledge.find(params[:id])
   end
 
   def edit
+      @beauty_nutrition_knowledge = BeautyNutritionKnowledge.find(params[:id])
   end
 
   def update
+    beauty_nutrition_knowledge = BeautyNutritionKnowledge.find(params[:id])
+    beauty_nutrition_knowledge.update(beauty_nutrition_knowledge_params)
+    redirect_to admin_beauty_nutrition_knowledges_path(beauty_nutrition_knowledge.id)
+  end
+
+  def destroy
+    beauty_nutrition_knowledge = BeautyNutritionKnowledge.find(params[:id])  # データ（レコード）を1件取得
+    beauty_nutrition_knowledge.destroy  # データ（レコード）を削除
+    redirect_to admin_beauty_nutrition_knowledges_path  # 投稿一覧画面へリダイレクト
   end
 
 
